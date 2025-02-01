85d7844791e2d1defa16dccbbf921cc76408217a
Vinh <vinh@avaprotocol.org>
2025-01-31 22:30:33 -0800

    use string instead of Date for sig gen function

--- packages/sdk-js/src/auth.ts
+++ packages/sdk-js/src/auth.ts
@@ -1,11 +1,11 @@
-export const getKeyRequestMessage = (chainId: number, address: string, issuedAt: Date, expiredAt: Date): string => {
+export const getKeyRequestMessage = (chainId: number, address: string, issuedAt: string, expiredAt: string): string => {
   return `Please sign the below text for ownership verification.
 
 URI: https://app.avaprotocol.org
 Chain ID: ${chainId}
 Version: 1
-Issued At: ${issuedAt.toISOString()}
-Expire At: ${expiredAt.toISOString()}
+Issued At: ${issuedAt}
+Expire At: ${expiredAt}
 Wallet: ${address}`;
 
 }
